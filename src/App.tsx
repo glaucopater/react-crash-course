3e6a139db143599a8b3b453ec9eb2972d0e39209
Glauco Pater <glauco.pater@gmail.com>
2020-09-02 19:47:13 +0200

    Part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import "./styles.css";
+import { Card } from "./components/Card";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Reaact Crash Course</h1>
+      <h2>Part 1</h2>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card/template.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./style.css";
+
+export const Card = () => {
+  return (
+    <div className="Card">
+      <h1>This is a cart</h1>
+      <p>Lorem ipsum bla bla bla</p>
+    </div>
+  );
+};
