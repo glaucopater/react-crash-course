52beabab9c9277fe5833538b7adf468fff600b0d
Glauco Pater <glauco.pater@gmail.com>
2020-09-03 21:31:31 +0200

    Refactor styling

--- src/components/Card/template.tsx
+++ src/components/Card/template.tsx
@@ -6,9 +6,8 @@ export const Card = (props: { value: number }) => {
   const handleOnClick = () => {
     console.log("click on card: " + value);
   };
-  const cssClasses = ["Card", ""];
   return (
-    <div className={cssClasses.join(" ")} onClick={handleOnClick}>
+    <div className="Card" onClick={handleOnClick}>
       <h3>This is a card {value}</h3>
       <p>Lorem ipsum bla bla bla</p>
     </div>
