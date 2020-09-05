8726668c9a2eb1a5e58d8555e52368e61c572ce6
Glauco Pater <glauco.pater@gmail.com>
2020-09-05 19:53:56 +0200

    Add Cards Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import "./styles.css";
+import { Deck } from "./containers/Deck";
 import { Card } from "./components/Card";
 
 export default function App() {
+  const cards = Array.from(Array(3).keys()).map((item) => (
+    <Card key={item} value={item} />
+  ));
+
   return (
     <div className="App">
       <h1>React Crash Course</h1>
       <h2>Session 1</h2>
-      <Card value={1} />
-      <Card value={2} />
+      <Deck cards={cards} />
     </div>
   );
 }
